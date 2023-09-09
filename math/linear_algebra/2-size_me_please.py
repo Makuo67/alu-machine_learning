1838647c8f0216e13cd4b6d7fef0ae71c8abd7da
Okeke Makuochukwu <99495812+Makuo67@users.noreply.github.com>
2023-09-10 01:28:05 +0200

    ok

---
+++ math/linear_algebra/2-size_me_please.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""Calculates the shape of a matrix"""
+
+def matrix_shape(matrix):
+    if not matrix:
+        return []
+
+    shape = []
+
+    while isinstance(matrix, list):
+        shape.append(len(matrix))
+        matrix = matrix[0]
+
+    return shape
