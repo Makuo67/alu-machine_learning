c2216235c60052eb9cb904226d2a2f8dbd530c27
SmartIsrael <okekemakuo67@gmail.com>
2023-09-10 04:16:48 +0200

    Trying 11

--- math/linear_algebra/11-the_western_exchange.py
+++ math/linear_algebra/11-the_western_exchange.py
@@ -2,22 +2,9 @@
 
 """Function that transposes a matrix"""
 
-import numpy as np
-
 
 def np_transpose(matrix):
     """Transposes a matrix"""
+    import numpy as np
     t = np.transpose(matrix)
     return t
-
-
-mat1 = np.array([1, 2, 3, 4, 5, 6])
-mat2 = np.array([])
-mat3 = np.array([[[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]],
-                 [[11, 12, 13, 14, 15], [16, 17, 18, 19, 20]]])
-print(np_transpose(mat1))
-print(mat1)
-print(np_transpose(mat2))
-print(mat2)
-print(np_transpose(mat3))
-print(mat3)
