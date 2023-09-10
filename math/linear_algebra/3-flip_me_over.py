1cd5e751555ad6069b894ba25dd537bdeffb48d7
SmartIsrael <okekemakuo67@gmail.com>
2023-09-10 02:12:23 +0200

    Task 3 works

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -5,16 +5,10 @@
 
 def matrix_transpose(matrix):
     """Returns the transpose"""
-    import numpy as np
-    matrix = np.array(matrix)
-    matrix = matrix.transpose()
-    return matrix
-
-
-# mat1 = [[1, 2], [3, 4]]
-# print(mat1)
-# print(matrix_transpose(mat1))
-# mat2 = [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15],
-#         [16, 17, 18, 19, 20], [21, 22, 23, 24, 25], [26, 27, 28, 29, 30]]
-# print(mat2)
-# print(matrix_transpose(mat2))
+    transpose = []
+    for i in range(len(matrix[0])):
+        transpose.append([])
+    for row in matrix:
+        for i in range(len(row)):
+            transpose[i].append(row[i])
+    return transpose
