a214895eb6a053e2de4748c2fc5dfd7764f5def6
SmartIsrael <okekemakuo67@gmail.com>
2023-09-10 04:19:28 +0200

    Trying 12

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -5,9 +5,6 @@
 
 def np_elementwise(mat1, mat2):
     """Performs some arithmetic"""
-    if mat1.shape != mat2.shape:
-        raise ValueError("Input matrices must have the same shape.")
-
     add = mat1 + mat2
     sub = mat1 - mat2
     mul = mat1 * mat2
