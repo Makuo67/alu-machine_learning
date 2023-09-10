6d417517117a39f60fdd7e2d04b925614c0b1543
SmartIsrael <okekemakuo67@gmail.com>
2023-09-10 03:20:03 +0200

    Task 7 is really cozy

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -14,17 +14,3 @@ def cat_matrices2D(mat1, mat2, axis=0):
             return None
         return [mat1[i] + mat2[i] for i in range(len(mat1))]
     return None
-
-
-# mat1 = [[1, 2], [3, 4]]
-# mat2 = [[5, 6]]
-# mat3 = [[7], [8]]
-# mat4 = cat_matrices2D(mat1, mat2)
-# mat5 = cat_matrices2D(mat1, mat3, axis=1)
-# print(mat4)
-# print(mat5)
-# mat1[0] = [9, 10]
-# mat1[1].append(5)
-# print(mat1)
-print(mat4)
-print(mat5)
