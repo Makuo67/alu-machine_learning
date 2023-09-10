8c1a61c8344751c4f2cc2c8d54ab21ddfbcc95b3
SmartIsrael <okekemakuo67@gmail.com>
2023-09-10 03:09:45 +0200

    Update to task 6

--- math/linear_algebra/6-howdy_partner.py
+++ math/linear_algebra/6-howdy_partner.py
@@ -5,18 +5,11 @@
 
 def cat_arrays(arr1, arr2):
     """Join two arrays"""
-    new_array = []
-    for i in arr1:
-        new_array.append(i)
-
-    for j in arr2:
-        new_array.append(j)
-
-    return new_array
+    return arr1 + arr2
 
 
-arr1 = [1, 2, 3, 4, 5]
-arr2 = [6, 7, 8]
-print(cat_arrays(arr1, arr2))
-print(arr1)
-print(arr2)
+# arr1 = [1, 2, 3, 4, 5]
+# arr2 = [6, 7, 8]
+# print(cat_arrays(arr1, arr2))
+# print(arr1)
+# print(arr2)
