b5bb98749727b69ed913126ce3c1e8f35f1dd0b5
SmartIsrael <okekemakuo67@gmail.com>
2023-09-10 04:43:01 +0200

    Updating other advanced tasks

---
+++ math/linear_algebra/101-the_whole_barn.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""FUnction that adds two matrices"""
+import numpy as np
+
+
+def add_matrices(mat1, mat2):
+    # Check if mat1 and mat2 have the same shape
+    if mat1.shape != mat2.shape:
+        return None
+
+    # Perform element-wise addition of the two matrices
+    result = mat1 + mat2
+
+    return result
---
+++ math/linear_algebra/102-squashed_like_sardines.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+"""Function that concatenates two matrices along a specific axis"""
+
+import numpy as np
+import time
+
+
+def cat_matrices(mat1, mat2, axis=0):
+    """Function"""
+    try:
+        # Check if mat1 and mat2 have the same shape along the specified axis
+        if mat1.shape[:axis] != mat2.shape[:axis] or mat1.shape[axis+1:] != mat2.shape[axis+1:]:
+            return None
+
+        # Use numpy's concatenate function to concatenate the matrices along the specified axis
+        result = np.concatenate((mat1, mat2), axis=axis)
+
+        return result
+    except ValueError:
+        # Handle ValueError if concatenation is not possible
+        return None
