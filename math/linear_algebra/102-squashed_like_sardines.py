d5bb1cf457eff4a6214e68fe9bb23cc117bd30a8
SmartIsrael <okekemakuo67@gmail.com>
2023-09-10 04:53:33 +0200

    Updating

--- math/linear_algebra/102-squashed_like_sardines.py
+++ math/linear_algebra/102-squashed_like_sardines.py
@@ -6,15 +6,11 @@
 def cat_matrices(mat1, mat2, axis=0):
     """Function"""
     import numpy as np
-    try:
-        # Check if mat1 and mat2 have the same shape along the specified axis
-        if mat1.shape[:axis] != mat2.shape[:axis] or mat1.shape[axis+1:] != mat2.shape[axis+1:]:
-            return None
 
-        # Use numpy's concatenate function to concatenate the matrices along the specified axis
-        result = np.concatenate((mat1, mat2), axis=axis)
-
-        return result
-    except ValueError:
-        # Handle ValueError if concatenation is not possible
+    if np.shape(mat1) != np.shape(mat2):
         return None
+
+    # Use numpy's concatenate function to concatenate the matrices along the specified axis
+    result = np.concatenate((mat1, mat2), axis=axis)
+
+    return result
