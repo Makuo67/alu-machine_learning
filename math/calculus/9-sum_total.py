6c5ca5cd42956e1a6ab5f18ccc2fca9432b2ed9e
Okeke Makuochukwu <99495812+Makuo67@users.noreply.github.com>
2023-09-22 12:07:11 +0200

    9-sum_total.py

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Sigma"""
 
+
 def summation_i_squared(n):
     """Summation"""
     # Check if n is a valid number (integer)
@@ -10,10 +11,8 @@ def summation_i_squared(n):
     # Base case: when n reaches 1, return 1^2 = 1
     if n == 1:
         return 1
-    
     # Recursive case: sum i^2 from 1 to n
     return (n**2 + summation_i_squared(n-1))
 
 # n = 5
-# print(summation_i_squared(n))
-        
+# print(summation_i_squared(n))       
