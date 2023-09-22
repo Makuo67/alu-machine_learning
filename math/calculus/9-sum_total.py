2aed7316b387737a853407fb93fc3a8232fbfff1
SmartIsrael <okekemakuo67@gmail.com>
2023-09-22 12:20:16 +0200

    9-sum total

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -7,12 +7,10 @@ def summation_i_squared(n):
     # Check if n is a valid number (integer)
     if not isinstance(n, int) or n < 1:
         return None
-    
+
     # Base case: when n reaches 1, return 1^2 = 1
     if n == 1:
         return 1
     # Sum of natural numbers
-    return ([n(n+1)(2n+1)]/6)
-
-# n = 5
-# print(summation_i_squared(n))       
+    formula = n*(n+1)((2*n)+1)/6
+    return formula
