483ec1003401721ca2a2b743eb26f8c452967e6c
Okeke Makuochukwu <99495812+Makuo67@users.noreply.github.com>
2023-09-22 12:15:12 +0200

    9-sum_total.py

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -11,8 +11,8 @@ def summation_i_squared(n):
     # Base case: when n reaches 1, return 1^2 = 1
     if n == 1:
         return 1
-    # Recursive case: sum i^2 from 1 to n
-    return (n**2 + summation_i_squared(n-1))
+    # Sum of natural numbers
+    return ([n(n+1)(2n+1)]/6)
 
 # n = 5
 # print(summation_i_squared(n))       
