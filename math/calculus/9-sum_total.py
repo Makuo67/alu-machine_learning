6bc19af68250eabeaaf8550e5582bad1258640bb
SmartIsrael <okekemakuo67@gmail.com>
2023-09-22 12:26:43 +0200

    9-sum total

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -12,5 +12,8 @@ def summation_i_squared(n):
     if n == 1:
         return 1
     # Sum of natural numbers
-    formula = n*(n+1)((2*n)+1)/6
-    return formula
+    return (n*(n+1)*(2*n+1))/6
+
+
+n = 5
+print(summation_i_squared(n))
