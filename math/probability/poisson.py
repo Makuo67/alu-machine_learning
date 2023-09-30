56df6ab2ec03ad5a9bd7e5cbbf42898623fbc0f5
Makuo67 <okekemakuo67@gmail.com>
2023-09-30 14:34:50 +0200

    probaility

---
+++ math/probability/poisson.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""Poisson distribution"""
+
+
+import math  # Needed for exp and factorial
+
+
+class Poisson:
+    def __init__(self, data=None, lambtha=1.):
+        if data is None:
+            if lambtha <= 0:
+                raise ValueError("lambtha must be a positive value")
+            else:
+                self.lambtha = float(lambtha)
+        else:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            self.lambtha = float(sum(data) / len(data))
+
+    def pmf(self, k):
+        k = int(k)  # Convert k to an integer if it is not
+        if k < 0:  # k is out of range
+            return 0
+        return (math.exp(-self.lambtha) * (self.lambtha ** k)) / math.factorial(k)
