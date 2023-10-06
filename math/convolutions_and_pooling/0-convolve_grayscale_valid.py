423af2fa915d854f42b785d91a94311c92b46a89
Makuo67 <okekemakuo67@gmail.com>
2023-10-06 03:25:37 +0200

    valid convolution

--- math/convolutions_and_pooling/0-convolve_grayscale_valid.py
+++ math/convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -8,16 +8,16 @@ def convolve_grayscale_valid(images, kernel):
     m, h, w = images.shape
     kh, kw = kernel.shape
 
+    grayscale_images = 0.2989 * images[:, :, :, 0] + 0.5870 * images[:, :, :, 1] + 0.1140 * images[:, :, :, 2]
+
     # Calculate the output image dimensions
     oh = h - kh + 1
     ow = w - kw + 1
 
     output = np.zeros((m, oh, ow))
 
-    for i in range(m):
-        for y in range(oh):
-            for x in range(ow):
-                patch = images[i, y: y + kh, x: x + kw]
-                output[i, y, x] = np.sum(patch * kernel)
-
+    for i in range(oh):
+        for j in range(ow):
+            output[:, i, j] = np.sum(grayscale_images[:, i: i + kh, j: j + kw] * kernel, axis=(1, 2))
+    
     return output
