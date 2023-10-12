33dee2099cf88d3065e0e744c72a39020eeaf947
Makuo67 <okekemakuo67@gmail.com>
2023-10-13 01:13:08 +0200

    checking for the 2 failing fix

--- math/linear_algebra/102-squashed_like_sardines.py
+++ math/linear_algebra/102-squashed_like_sardines.py
@@ -9,6 +9,10 @@ def cat_matrices(mat1, mat2, axis=0):
     if not isinstance(mat1, list) or not isinstance(mat2, list):
         return None
 
+     # If the depth of the two matrices are different, return None
+    if isinstance(mat1[0], list) != isinstance(mat2[0], list):
+        return None
+
     # When axis is 0, concatenate the two matrices directly
     if axis == 0:
         return mat1 + mat2
