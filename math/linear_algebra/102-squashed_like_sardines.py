e31b846f620f545fce43f4b8448c45f61d342a98
Makuo67 <okekemakuo67@gmail.com>
2023-10-13 00:56:14 +0200

    check 102

--- math/linear_algebra/102-squashed_like_sardines.py
+++ math/linear_algebra/102-squashed_like_sardines.py
@@ -10,13 +10,6 @@ def cat_matrices(mat1, mat2, axis=0):
     if type(mat1[0]) not in [list, tuple] and axis == 0:
         return mat1 + mat2
 
-    # Check for dimensional mismatch in lists before the specified axis
-    for i in range(axis):
-        if len(mat1) != len(mat2):
-            return None
-        mat1 = mat1[0]
-        mat2 = mat2[0]
-
     if axis == 0:
         if len(mat1[0]) != len(mat2[0]):
             return None
@@ -25,9 +18,17 @@ def cat_matrices(mat1, mat2, axis=0):
     if axis == 1:
         if len(mat1) != len(mat2):
             return None
-        return [cat_matrices(m1, m2, axis=0) for m1, m2 in zip(mat1, mat2)]
+        return [cat_matrices(
+            mat1[i], mat2[i], axis=0) for i in range(len(mat1))]
+
+    for i in range(axis):
+        if len(mat1) != len(mat2):
+            return None
+        mat1 = mat1[0]
+        mat2 = mat2[0]
 
     # Recursive case for multi-dimensional lists
     if len(mat1) != len(mat2):
         return None
-    return [cat_matrices(m1, m2, axis=axis-1) for m1, m2 in zip(mat1, mat2)]
+    return [cat_matrices(
+        mat1[i], mat2[i], axis=axis-1) for i in range(len(mat1))]
