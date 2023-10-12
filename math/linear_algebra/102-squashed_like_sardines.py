1b1076048dcb3a774d02945f47886ffc86221d33
Makuo67 <okekemakuo67@gmail.com>
2023-10-13 01:09:08 +0200

    fixing 102 concatenation errors

--- math/linear_algebra/102-squashed_like_sardines.py
+++ math/linear_algebra/102-squashed_like_sardines.py
@@ -9,29 +9,20 @@ def cat_matrices(mat1, mat2, axis=0):
     if not isinstance(mat1, list) or not isinstance(mat2, list):
         return None
 
-    # Base case for 1D list (or the deepest dimension in multi-D lists)
-    if type(mat1[0]) not in [list, tuple] and axis == 0:
-        return mat1 + mat2
-
+    # When axis is 0, concatenate the two matrices directly
     if axis == 0:
-        if len(mat1[0]) != len(mat2[0]):
-            return None
         return mat1 + mat2
 
-    if axis == 1:
-        if len(mat1) != len(mat2):
-            return None
-        return [cat_matrices(
-            mat1[i], mat2[i], axis=0) for i in range(len(mat1))]
-
-    for i in range(axis):
-        if len(mat1) != len(mat2):
-            return None
-        mat1 = mat1[0]
-        mat2 = mat2[0]
-
-    # Recursive case for multi-dimensional lists
+    # For axis > 0, check if the current dimensions are the same
     if len(mat1) != len(mat2):
         return None
-    return [cat_matrices(
-        mat1[i], mat2[i], axis=axis-1) for i in range(len(mat1))]
+
+    # Recursively concatenate the next dimension down
+    concatenated = []
+    for a, b in zip(mat1, mat2):
+        concatenated_part = cat_matrices(a, b, axis - 1)
+        if concatenated_part is None:
+            return None
+        concatenated.append(concatenated_part)
+
+    return concatenated
