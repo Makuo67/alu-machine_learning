8ad7942fde9ccf16621cd1c3011dcb5c83faa07b
Makuo67 <okekemakuo67@gmail.com>
2023-10-22 01:25:03 +0200

    fixing this dimension error

--- math/multivariate_prob/multinormal.py
+++ math/multivariate_prob/multinormal.py
@@ -1,54 +1,74 @@
 #!/usr/bin/env python3
-"""Defines `mean_cov`."""
+"""
+Defines class MultiNormal that represents a Multivariate Normal Distribution
+"""
+
+
 import numpy as np
 
 
 class MultiNormal:
-    """A multivariate normal distribution."""
+    """
+    Class that represents Multivariate Normal Distribution
+
+    class constructor:
+        def __init__(self, data)
+
+    public instance variables:
+        mean [numpy.ndarray of shape (d, 1)]:
+            contains the mean of data
+        cov [numpy.ndarray of shape (d, d)]:
+            contains the covariance matrix of data
+
+    public instance method:
+        def pdf(self, x):
+            calculates the PDF at a data point
+    """
 
     def __init__(self, data):
         """
-        Initializes a MultiNormal.
+        Class constructor
 
-        data: A numpy.ndarray of shape (d, n) containing the data set:
-            d: The number of dimensions in each data point
-            n: The number of data points
+        parameters:
+            data [numpy.ndarray of shape (d, n)]:
+                contains the data set
+                d: number of dimensions in each data point
+                n: number of data points
         """
-        if type(data) != np.ndarray or len(data.shape) != 2:
+        if type(data) is not np.ndarray or len(data.shape) != 2:
             raise TypeError("data must be a 2D numpy.ndarray")
-        elif data.shape[1] < 2:
-            raise ValueError('data must contain multiple data points')
-
-        sample_count = data.shape[1]
-        self.mean = np.mean(data, axis=1, keepdims=True)
-        mu = self.mean
-        self.cov = np.matmul(data - mu, data.T - mu.T) / (sample_count - 1)
+        d, n = data.shape
+        if n < 2:
+            raise ValueError("data must contain multiple data points")
+        mean = np.mean(data, axis=1, keepdims=True)
+        self.mean = mean
+        cov = np.matmul(data - mean, data.T - mean.T) / (n - 1)
+        self.cov = cov
 
     def pdf(self, x):
         """
-        Calculates the probability density at a data point.
+        Calculates the PDF at a data point
 
-        x: A numpy.ndarray of shape (d, 1) containing the data point whose PDF
-            should be calculated:
-            d: The number of dimensions of the MultiNormal instance
+        parameters:
+            x [numpy.ndarray of shape (d, 1)]:
+                contains the data point whose PDF should be calculated
+                d: number of dimensions of the instance
 
-        Returns: The value of the PDF at a point.
+        returns:
+            value of the PDF
         """
-        if not type(x) is np.ndarray:
-            raise TypeError('x must be a numpy.ndarray')
+        if type(x) is not np.ndarray:
+            raise TypeError("x must be a numpy.ndarray")
         d = self.cov.shape[0]
         if len(x.shape) != 2:
             raise ValueError("x must have the shape ({}, 1)".format(d))
-        d0, d1 = x.shape
-        if d0 != d or d1 != 1:
+        test_d, one = x.shape
+        if test_d != d or one != 1:
             raise ValueError("x must have the shape ({}, 1)".format(d))
-
-        tau = np.pi * 2
-        determinant = np.linalg.det(self.cov)
-        x_centered = x - self.mean
-        # Using np.linalg.solve() for more stability
-        exp_term = np.dot(x_centered.T, np.linalg.solve(self.cov, x_centered))
-        probability_density = np.exp(-0.5 * exp_term) / \
-            np.sqrt(tau ** d * determinant)
-
-        return np.asscalar(probability_density)
+        det = np.linalg.det(self.cov)
+        inv = np.linalg.inv(self.cov)
+        pdf = 1.0 / np.sqrt(((2 * np.pi) ** d) * det)
+        mult = np.matmul(np.matmul((x - self.mean).T, inv), (x - self.mean))
+        pdf *= np.exp(-0.5 * mult)
+        pdf = pdf[0][0]
+        return pdf
