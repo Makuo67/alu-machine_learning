11b2ce4ede5897dcf5cc48da2279223f9a64235b
Makuo67 <okekemakuo67@gmail.com>
2023-11-02 16:32:17 +0200

    syntax error check

--- math/bayesian_prob/1-intersection.py
+++ math/bayesian_prob/1-intersection.py
@@ -32,9 +32,9 @@ def intersection(x, n, P, Pr):
     if not isinstance(Pr, np.ndarray) or Pr.ndim != 1 or Pr.shape != P.shape:
         raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
     if any(val < 0 or val > 1 for val in P):
-        raise ValueError(f"All values in P must be in the range [0, 1]")
+        raise ValueError("All values in P must be in the range [0, 1]")
     if any(val < 0 or val > 1 for val in Pr):
-        raise ValueError(f"All values in Pr must be in the range [0, 1]")
+        raise ValueError("All values in Pr must be in the range [0, 1]")
     if not np.isclose(np.sum(Pr), 1):
         raise ValueError("Pr must sum to 1")
 
