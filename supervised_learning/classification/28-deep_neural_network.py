fix calculation

supervised_learning/classification/28-deep_neural_network.py
@@ -77,7 +77,7 @@ class DeepNeuralNetwork:
     def cost(self, Y, A):
         m = Y.shape[1]
         epsilon = 1e-15
-        cost = -np.sum(Y * np.log(A + epsilon)) / m
+        cost = -np.sum(Y * np.log(A + epsilon) + (1 - Y) * np.log(1 - A + epsilon)) / m
         return cost
 
     def evaluate(self, X, Y):