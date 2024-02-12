a358e1b0de92a292eb93ed9f57aabb71d7e2aaf1
Makuo67 <okekemakuo67@gmail.com>
2024-02-12 18:41:10 +0200

    confusion matrix created

--- supervised_learning/error_analysis/0-create_confusion.py
+++ supervised_learning/error_analysis/0-create_confusion.py
@@ -1,28 +1,19 @@
 #!/usr/bin/env python3
-"""Error Analysis"""
+"""Create confusion"""
 import numpy as np
 
 
 def create_confusion_matrix(labels, logits):
-    """
-    Creates a confusion matrix.
+    """Creates a confusion matrix"""
+    m, classes = labels.shape
+    result = np.zeros((classes, classes))
 
-    Args:
-    - labels: one-hot numpy.ndarray of shape (m, classes) containing the correct labels for each data point
-    - logits: one-hot numpy.ndarray of shape (m, classes) containing the predicted labels
+    for i in range(m):
+        sub_label = labels[i]
+        sub_logits = logits[i]
 
-    Returns:
-    - confusion: numpy.ndarray of shape (classes, classes) with row indices representing the correct labels
-                 and column indices representing the predicted labels
-    """
-    # Convert one-hot encoded labels and logits to class indices
-    true_labels = np.argmax(labels, axis=1)
-    predicted_labels = np.argmax(logits, axis=1)
+        x, *_ = np.where(sub_label == 1)
+        y, *_ = np.where(sub_logits == 1)
+        result[x[0]][y[0]] += 1
 
-    # Calculate the confusion matrix
-    confusion = np.zeros((labels.shape[1], labels.shape[1]), dtype=np.int)
-
-    for true_label, predicted_label in zip(true_labels, predicted_labels):
-        confusion[true_label, predicted_label] += 1
-
-    return confusion
+    return result
