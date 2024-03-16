cf16d983b1cde511f431d6325b2b21cf094fcb19
Makuo67 <okekemakuo67@gmail.com>
2024-03-16 16:47:29 +0200

    update 16

--- pipeline/databases/16-glam_rock.sql
+++ pipeline/databases/16-glam_rock.sql
@@ -1,5 +1,5 @@
 -- List all bands of Glam rock
 SELECT band_name, IF(split IS NULL, YEAR(CURDATE()), split) - formed AS lifespan
 FROM  metal_bands
-WHERE style LIKE "%Glam rock%"
+WHERE style LIKE "%Glam Rock%"
 ORDER BY lifespan DESC;
