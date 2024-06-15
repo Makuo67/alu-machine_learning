ad8d29a68a16c3a330d8d90ded34dc5921a1701b
Makuo67 <okekemakuo67@gmail.com>
2024-06-15 12:37:53 +0200

    update to rnn encoder

--- supervised_learning/attention/0-rnn_encoder.py
+++ supervised_learning/attention/0-rnn_encoder.py
@@ -1,42 +1,31 @@
 #!/usr/bin/env python3
+""" doc """
 
-"""
-This module contains a class RNNEncoder
-that encode machine translation"""
 import tensorflow as tf
 
 
 class RNNEncoder(tf.keras.layers.Layer):
-    """class RNNEncoder"""
+    """ doc """
 
     def __init__(self, vocab, embedding, units, batch):
+        """ doc """
         super(RNNEncoder, self).__init__()
-        """class contructor
-        vocab - int size of input vocabulary
-        embedding - dim of embedding vector
-        units -  no. of hidden units in RNN cell
-        batch - batch size int"""
-        self.vocab = vocab
-        self.embed = embedding
-        self.units = units
         self.batch = batch
+        self.units = units
         self.embedding = tf.keras.layers.Embedding(input_dim=vocab,
-                                                   output_dim=self.embed)
+                                                   output_dim=embedding)
         self.gru = tf.keras.layers.GRU(units=units,
-                                       kernel_initializer='glorot_uniform',
+                                       recurrent_initializer='glorot_uniform',
                                        return_sequences=True,
                                        return_state=True)
 
     def initialize_hidden_state(self):
-        """
-        initialize hidden states"""
-        return tf.zeros(shape=(self.batch, self.units))
+        """ doc """
+        init = tf.keras.initializers.Zeros()
+        return (init(shape=(self.batch, self.units)))
 
     def call(self, x, initial):
-        """
-        x - tensor - (batch, input_seq_len)
-        initial - tensor-(batch, units) - intial hidden state
-        """
-        x = self.embedding(x)
-        outputs, hidden = self.gru(x, initial_state=initial)
+        """ doc """
+        embedding = self.embedding(x)
+        outputs, hidden = self.gru(embedding, initial_state=initial)
         return outputs, hidden
