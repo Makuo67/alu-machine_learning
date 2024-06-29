99241f4e0ac9a0bffef42db2a47774479b8670a0
Okeke Makuochukwu <99495812+Makuo67@users.noreply.github.com>
2024-06-30 00:39:43 +0200

    Update 7-maximization.py

--- unsupervised_learning/clustering/7-maximization.py
+++ unsupervised_learning/clustering/7-maximization.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
-
-"""
-This module contains a function that calculates
-maximization step in the EM algorithm for a GMM
+""" M-step: re-estimate all parameters so that
+the likelihood of observing what we observed is maximized
 """
 
 import numpy as np
@@ -10,33 +8,59 @@ import numpy as np
 
 def maximization(X, g):
     """
-    initializes variables for a Gaussian Mixture Model
-
-    X: numpy.ndarray (n, d) containing the dataset
-        - n no. of data points
-        - d no. of dimensions for each data point
-    g: numpy.ndarray (k, n) containing the posterior
-        probabilities for each data point in each cluster
-
-    return:
-        - pi: numpy.ndarray (k,) containing the updated priors for each cluster
-        - m: numpy.ndarray (k, d) has updated centroid means for each cluster
-        - S: numpy.ndarray (k, d, d) updated cov matrices for each cluster
+    calculates the maximization step in the EM algorithm for a GMM
+    Args:
+        X: numpy.ndarray of shape (n, d) containing the data set
+        g: numpy.ndarray of shape (k, n) containing the posterior
+           probabilities for each data point in each cluster
+    Returns: pi, m, S, or None, None, None on failure
+             pi: numpy.ndarray of shape (k,) containing the updated priors
+                 for each cluster
+             m: numpy.ndarray of shape (k, d) containing the updated centroid
+                means for each cluster
+             S: numpy.ndarray of shape (k, d, d) containing the updated
+                covariance matrices for each cluster
     """
     if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         return None, None, None
-    if not isinstance(g, np.ndarray) or len(g.shape) != 2:
+    if not isinstance(g, np.ndarray) or len(X.shape) != 2:
         return None, None, None
+
+    gaussian_components = g
+
+    k = gaussian_components.shape[0]
     n, d = X.shape
-    k = g.shape[0]
-    if n != g.shape[1]:
+
+    posterior_prob = np.sum(gaussian_components, axis=0)
+    check = np.sum(posterior_prob)
+    if check != X.shape[0]:
         return None, None, None
-    if not np.isclose(np.sum(g, axis=0), 1).all():
-        return None, None, None
-    pi = np.sum(g, axis=1) / n
-    m = np.dot(g, X) / np.sum(g, axis=1)[:, np.newaxis]
-    S = np.zeros((k, d, d))
+
+    # pi
+    priors = np.zeros((k,))
+
+    # m
+    centroid_updated = np.zeros((k, d))
+
+    # S
+    covariance_updated = np.zeros((k, d, d))
+
+    # Formula https://bit.ly/31pkdox
     for i in range(k):
-        y = X - m[i]
-        S[i] = np.dot(g[i] * y.T, y) / np.sum(g[i])
-    return pi, m, S
+
+        # Mu components
+        # Needed to adjust dimensions for fitting the covariance
+        mu_up = np.sum((gaussian_components[i, :, np.newaxis] * X), axis=0)
+        mu_down = np.sum(gaussian_components[i], axis=0)
+        centroid_updated[i] = mu_up / mu_down
+
+        # Sigma components
+        x_m = X - centroid_updated[i]
+        sigma_up = np.matmul(gaussian_components[i] * x_m.T, x_m)
+        sigma_down = np.sum(gaussian_components[i])
+        covariance_updated[i] = sigma_up / sigma_down
+
+        # Pi =  priors after computing derivation of sigma and mu
+        # Formula: P(j) = n(j) / n = Σn i=1 P(j|i) / n
+        priors[i] = np.sum(gaussian_components[i]) / n
+    return priors, centroid_updated, covariance_updated
